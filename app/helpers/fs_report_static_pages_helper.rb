f0a13f5dbdd639019b2d052322e1bf55c27b021c
Josh Walton <joash.tron@gmail.com>
2019-02-26 16:42:06 -0600

    Boiler Plate Information Pages, Database, Routes

---
+++ app/helpers/fs_report_static_pages_helper.rb
@@ -0,0 +1,2 @@
+module FsReportStaticPagesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'fs_report_static_pages#home'
+
+  get 'fs_report_static_pages/home'
+  get 'fs_report_static_pages/help'
+  get 'fs_report_static_pages/about'
+  get 'fs_report_static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
