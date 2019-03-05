56d62c8f7328f8643637959d4ca03629e53e4a84
Josh Walton <joash.tron@gmail.com>
2019-03-05 11:12:39 -0600

    Sass and Css Tweaking with Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'fs_report_static_pages#home'
-
-  get 'fs_report_static_pages/home'
-  get 'fs_report_static_pages/help'
-  get 'fs_report_static_pages/about'
-  get 'fs_report_static_pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/help', to: 'fs_report_static_pages#help'
+  get '/about', to: 'fs_report_static_pages#about'
+  get '/contact', to: 'fs_report_static_pages#contact'
 end
+
