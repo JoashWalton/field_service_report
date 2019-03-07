248eb4e89c5f36e5427acd719718e1668ff8da9a
Josh Walton <joash.tron@gmail.com>
2019-03-07 16:43:06 -0600

    Users Controller Routes for Signing Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'fs_report_static_pages#home'
   get '/help', to: 'fs_report_static_pages#help'
   get '/about', to: 'fs_report_static_pages#about'
   get '/contact', to: 'fs_report_static_pages#contact'
+  get '/signup', to: 'users#new'
 end
 
